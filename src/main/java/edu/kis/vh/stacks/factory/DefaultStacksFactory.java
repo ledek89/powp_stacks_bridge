99e4386f9d9ca230ad01b2bbcc5b5e32fb0e5715
Blazej Lewandowski <ledek89@gmail.com>
2017-03-15 21:13:45 +0100

    Wykonany podpunkt 3.1.12

--- src/main/java/edu/kis/vh/stacks/factory/DefaultStacksFactory.java
+++ src/main/java/edu/kis/vh/stacks/factory/DefaultStacksFactory.java
@@ -1,9 +1,15 @@
 package edu.kis.vh.stacks.factory;
 
-import edu.kis.vh.stacks.factory.IStacksFactory;
 import edu.kis.vh.stacks.Stack;
 import edu.kis.vh.stacks.StackFIFO;
 import edu.kis.vh.stacks.StackHanoi;
+
+interface IStacksFactory {
+		Stack getStandardStack();
+		Stack getFalseStack();
+		StackFIFO getFIFOStack();
+		StackHanoi getHanoiStack();
+	}
 
 public class DefaultStacksFactory implements IStacksFactory {
 
--- src/main/java/edu/kis/vh/stacks/list/StackList.java
+++ src/main/java/edu/kis/vh/stacks/list/StackList.java
@@ -44,4 +44,35 @@ public class StackList {
 		this.last = last;
 	}
 
+class Node {
+
+		private final int value;		//po hermetyzacji pól, dostęp do nich z StackList odbywa się poprzez gettery i settery
+		private Node prev;
+		private Node next;
+
+		public Node(int i) {
+			value = i;
+		}
+
+		int getValue() {
+			return value;
+		}
+
+		Node getPrev() {
+			return prev;
+		}
+
+		void setPrev(Node prev) {
+			this.prev = prev;
+		}
+
+		Node getNext() {
+			return next;
+		}
+
+		void setNext(Node next) {
+			this.next = next;
+		}
+		
+	}
 }
