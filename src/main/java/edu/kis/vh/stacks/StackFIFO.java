b52f0b6042aed0bab6871938825398fc5449a6aa
Blazej Lewandowski <ledek89@gmail.com>
2017-03-23 03:47:38 +0100

    3.1.12

--- src/main/java/edu/kis/vh/stacks/StackFIFO.java
+++ src/main/java/edu/kis/vh/stacks/StackFIFO.java
@@ -1,6 +1,18 @@
 package edu.kis.vh.stacks;
 
+import edu.kis.vh.stacks.types.StackList;
+
 public class StackFIFO extends Stack {
+
+	public StackFIFO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StackFIFO(StackList stackList) {
+		super(stackList);
+		// TODO Auto-generated constructor stub
+	}
 
 	private final Stack temp = new Stack();
 	
--- src/main/java/edu/kis/vh/stacks/StackHanoi.java
+++ src/main/java/edu/kis/vh/stacks/StackHanoi.java
@@ -1,6 +1,18 @@
 package edu.kis.vh.stacks;
 
+import edu.kis.vh.stacks.types.StackList;
+
 public class StackHanoi extends Stack {
+
+public StackHanoi() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StackHanoi(StackList stackList) {
+		super(stackList);
+		// TODO Auto-generated constructor stub
+	}
 
 private int totalRejected = 0;
 
